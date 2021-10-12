177f4cb2ad53f494b633b5d2c8cee22088de67b9
waverDeep <dandyleader@naver.com>
2021-10-12 16:58:34 +0900

    pytorch에서 제공하는 distributedDataParallel 방법 구현 (오류가 있는 상태)

---
+++ main_DDP.py
@@ -0,0 +1,36 @@
+import torch
+import torch.distributed as distributed
+import torch.multiprocessing as multiprocessing
+import model.model_sample as models
+import torch.nn as nn
+import torch.optim as optim
+from torch.nn.parallel import DistributedDataParallel as DDP
+import os
+
+
+os.environ['MASTER_ADDR'] = 'localhost'
+os.environ['MASTER_PORT'] = '29500'
+
+
+def main(rank, gpu_size):
+    distributed.init_process_group(backend='nccl', rank=rank, world_size=gpu_size)
+
+    model = models.ResNet18()
+
+    ddp_model = DDP(model, device_ids=[rank])
+
+    loss_function = nn.MSELoss()
+    optimizer = optim.SGD(ddp_model.parameters(), lr=0.001)
+
+    preds = ddp_model(torch.randn(1, 3, 32, 32)).to(rank)
+    labels = torch.randn(1, 1).to(rank)
+
+    loss_function(preds, labels).backward()
+    optimizer.step()
+
+
+if __name__ == '__main__':
+    _gpu_size = torch.cuda.device_count()
+    print('gpu_size: ', _gpu_size)
+    _rank = 1
+    multiprocessing.spawn(main, args=(_rank, ), nprocs=_gpu_size, join=True)
